8f3d0d8025265fd62d078f77c5da41e97b6324fc
canberkelmal <elmalcanberk@gmail.com>
2023-01-10 00:24:35 +0300

    BetterGrouping

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,15 +13,16 @@ public class GameManager : MonoBehaviour
     #region variables
     GameObject Chars;
     public Joystick joystick;
-    float ForwardValue;
-    Rigidbody rb, tgRb;
+    Rigidbody rb;
     public float sensivity, CamSens, groupSens, stopDist;
     public int playerCount = 1;
-    NavMeshAgent PlayerNavMesh;
     GameObject cam, tg;
     public Vector3 camOffs;
+    float x, z;
+    Vector3 TargetPos;
 
-    [Range(0f,1f)] [SerializeField] private float DistanceFactor, Radius;
+    [Range(0f,1f)] [SerializeField] private float Dist, Radius;
+    float[] rads;
 
     #endregion
 
@@ -33,25 +34,20 @@ public class GameManager : MonoBehaviour
         //tg = GameObject.Find("Toggle");
         //tgRb = tg.GetComponent<Rigidbody>();
         rb = Chars.GetComponent<Rigidbody>();
+        rads = new float[] {UnityEngine.Random.Range(0, 1)};
     }
 
     // Update is called once per frame
     void Update()
     {
-        ClickDedector();
+        CameraController();
         SetPlayersPos();
-    }
-
-    void ClickDedector()
-    {
-
     }
 
     void FixedUpdate()
     {
-        CameraController();
+
         InputsController();
-        //PlayersController();
     }
 
     //Controls all inputs
@@ -74,10 +70,10 @@ public class GameManager : MonoBehaviour
     public void AddRemove(bool op, int opNumber)
     {
         //Adds players
-        if(op)
+        if (op)
         {
             float t = Chars.transform.childCount * 0.15f;
-            for(int i = 0; i<opNumber; i++)
+            for (int i = 0; i < opNumber; i++)
             {
                 Instantiate(Chars.transform.GetChild(0).gameObject,
                             Chars.transform.position,
@@ -86,29 +82,50 @@ public class GameManager : MonoBehaviour
             }
             camOffs += new Vector3(0, t, -t);
         }
-        //removes players
+
+        //Removes players
         else
         {
             int t = Chars.transform.childCount - 1;
-            for(int i = 0; i<opNumber; i++)
+            for (int i = 0; i < opNumber; i++)
             {
-                Destroy(Chars.transform.GetChild(t-i).gameObject);
+                Destroy(Chars.transform.GetChild(t - i).gameObject);
             }
             camOffs += new Vector3(0, t, -t);
         }
+
+
+        //Updates the playerCount according to the operation that done
         playerCount = Chars.transform.childCount;
+        rads = new float[playerCount];
+        for (int i = 0; i < rads.Length; i++)
+        {
+            rads[i] = UnityEngine.Random.Range(0, 100) * 0.01f;
+            Chars.transform.GetChild(i).GetComponent<NavMeshAgent>().avoidancePriority = i;
+        }
     }
 
+    //Sets collected players pos during update
     void SetPlayersPos()
     {
         for (int i = 0; i < Chars.transform.childCount; i++)
         {
-            var x = DistanceFactor * Mathf.Sqrt(i) * Mathf.Cos(i * Radius);
-            var z = DistanceFactor * Mathf.Sqrt(i) * Mathf.Sin(i * Radius);
-            var NewPos = new Vector3(x, Chars.transform.GetChild(i).position.y, z);
+            if (Chars.transform.GetChild(i).position.x < -4.5f || Chars.transform.GetChild(i).position.x > 4.5f)
+            {
+                Chars.transform.GetChild(i).GetComponent<CapsuleCollider>().isTrigger = true;
+                Chars.transform.GetChild(i).AddComponent<Rigidbody>();
+                Chars.transform.GetChild(i).SetParent(null);
+            }
+            else
+            {
+                float rnd = UnityEngine.Random.Range(0, Chars.transform.childCount);
+                x = Dist * Mathf.Sqrt(i) * Mathf.Cos(i * rads[i]);
+                z = Dist * Mathf.Sqrt(i) * Mathf.Sin(i * rads[i]);
+                TargetPos = new Vector3(x, Chars.transform.GetChild(i).localPosition.y, z);
 
-            if (i != 0)
-                Chars.transform.GetChild(i).localPosition = Vector3.MoveTowards(Chars.transform.GetChild(i).localPosition, NewPos, groupSens * Time.deltaTime);
+                if (i != 0)
+                    Chars.transform.GetChild(i).localPosition = Vector3.MoveTowards(Chars.transform.GetChild(i).localPosition, TargetPos, groupSens * Time.deltaTime);
+            }
         }
     }
 
