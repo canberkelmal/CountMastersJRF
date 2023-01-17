c546c572352578823287e04b0dfaf1dda435fbe6
canberkelmal <elmalcanberk@gmail.com>
2023-01-18 02:27:21 +0300

    CylinderDone

--- Assets/CylinderSc.cs
+++ Assets/CylinderSc.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CylinderSc : MonoBehaviour
 {
     GameManager gm;
+    public GameObject playerDeathSplashEffect;
+    public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -12,10 +15,16 @@ public class CylinderSc : MonoBehaviour
         gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
+    private void FixedUpdate()
+    {
+        transform.GetChild(0).Rotate(speed * Time.deltaTime, 0, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            Instantiate(playerDeathSplashEffect, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
             gm.SetPlayerCount();
         }
