73bb9bc11053afb79f30b7d1636e08b3c4557879
canberkelmal <elmalcanberk@gmail.com>
2023-01-27 21:33:03 +0300

    StairsDone

--- Assets/Scripts/StairSc.cs
+++ Assets/Scripts/StairSc.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StairSc : MonoBehaviour
 {
+    private void Start()
+    {
+        for(int i=0; i<transform.childCount; i++)
+        {
+            transform.GetChild(i).GetChild(0).GetChild(0).GetComponent<Text>().text = (i * 0.2f + 1).ToString();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
